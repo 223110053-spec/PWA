41fa998f2372800bfcfa918c38f02b93cb9492d6
223110053-spec <223110053@utvam.edu.mx>
2026-03-03 23:02:41 -0600

    actualizacion settings y archivos estaticos

--- proyectoweb3/dani/settings.py
+++ proyectoweb3/dani/settings.py
@@ -56,3 +56,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'core', 'static'),
+]
